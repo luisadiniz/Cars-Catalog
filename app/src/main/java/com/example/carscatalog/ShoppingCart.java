038196573c94e92b504d34572cb4464975c4d99f
Luisa Diniz Lopes <luisa.diniz07@gmail.com>
2019-10-12 15:55:29 -0300

    adding a car in the cart list

--- app/src/main/java/com/example/carscatalog/ShoppingCart.java
+++ app/src/main/java/com/example/carscatalog/ShoppingCart.java
@@ -11,34 +11,38 @@ import java.util.ArrayList;
 
 public class ShoppingCart extends AppCompatActivity {
 
+    ArrayList<Car> cartItems = new ArrayList<Car>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shopping_cart);
 
+        SetContentClicked();
     }
 
     public void SetContentClicked()
     {
         Intent getLastIntent = getIntent();
 
-//        Car carClicked = new Car();
-//        carClicked.nome = getLastIntent.getStringExtra("Name");
-//        carClicked.imagem = getLastIntent.getStringExtra("Image");
-//        carClicked.preco = getLastIntent.getStringExtra("Price");
-//        carClicked.id ="";
-//        carClicked.descricao="";
-//        carClicked.marca="";
-//        carClicked.quantidade="";
+        Car carClicked = new Car();
+        carClicked.nome = getLastIntent.getStringExtra("Name");
+        carClicked.imagem = getLastIntent.getStringExtra("Image");
+        carClicked.preco = getLastIntent.getStringExtra("Price");
+        carClicked.id ="";
+        carClicked.descricao="";
+        carClicked.marca="";
+        carClicked.quantidade="";
+
+        cartItems.add(carClicked);
+
+        CarsAdapter cartArray = new CarsAdapter(this, cartItems);
+        ListView listView = (ListView) findViewById(R.id.cart_list_view);
+        listView.setAdapter(cartArray);
     }
 
     public void EndShopping(View view)
     {
         Toast.makeText(getApplicationContext(), "Compra Finalizada!", Toast.LENGTH_LONG).show();
     }
-
-//    public void PopulateList()
-//    {
-//        ListView listView = (ListView) findViewById(R.id.cart_list_view);
-//    }
 }
